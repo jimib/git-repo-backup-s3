821416f93e4443cd439c77727809859b157ea8b3
James Bailey <jimi.bailey@gmail.com>
2013-05-15 15:24:26 +0100

    Reverted to previous version on old server - fault in script unable to pin down

--- backup.py
+++ backup.py
@@ -21,8 +21,8 @@ emailUser = "<EMAIL_USERNAME>"
 emailPassword = "<EMAIL_PASSWORD>"
 
 def percent_cb(complete, total):
-	# pass
-	print complete, total
+	pass
+	# print complete, total
 
 print 'Connecting to S3...'
 
@@ -31,10 +31,10 @@ conn = S3Connection(AWS_KEY, AWS_SECRET)
 print 'Connected to S3...'
 
 #link to bucket
+bucketName = 'jimib-backups'
 print 'Open bucket "' + bucketName + '"...'
 bucket = conn.get_bucket(bucketName)
 print 'Opened bucket "' + bucketName + '"...'
-
 
 def create_cache(fullpath, cachepath):
 	cachedir = os.path.dirname(cachepath)
@@ -59,29 +59,28 @@ def upload_cache(cachepath, target):
 	k.close()
 
 def is_cache_in_date(originalpath, cachepath):
+	
+	if not os.path.exists(cachepath):
+		return False
 
-        if not os.path.exists(cachepath):
-                return False
+	mtime_origin = os.stat(originalpath).st_mtime
+	mtime_cache = os.stat(cachepath).st_mtime
+	if mtime_origin > mtime_cache:
+		return False
+	elif has_directory_been_modified_since(originalpath, mtime_cache):
+		return False
 
-        mtime_origin = os.stat(originalpath).st_mtime
-        mtime_cache = os.stat(cachepath).st_mtime
-
-		#make sure the cache is more than 30seconds out date in case we downloaded as part of a restore
-        if mtime_origin > mtime_cache + 30000:
-                return False
-        elif has_directory_been_modified_since(originalpath, mtime_cache):
-                return False
-
-        return True
+	return True
 
 def has_directory_been_modified_since(path, mtime):
-        for root, dirnames, files in os.walk(path):
-                if os.stat(root).st_mtime > mtime:
-                        return True
+	for root, dirnames, files in os.walk(path):
+		if os.stat(root).st_mtime > mtime:
+			return True
 
-        return False
+	return False
 
 #check each repository - if no cache or cache is out dated then we need to re-upload it
+arrUpdatedRepos = []
 for root, dirnames, files in os.walk(pathRootRepository):
 	rootName = os.path.basename(root)
 	fileName, fileExtension = os.path.splitext(rootName)
@@ -89,53 +88,56 @@ for root, dirnames, files in os.walk(pathRootRepository):
 		gitName = os.path.relpath(root, pathRootRepository)
 		pathRel = gitName + ".tar.gz"
 		cachepath = os.path.join(pathRootCache, pathRel)
+
+		#get the last set of commits
+		git = Git(root)
 		
-		#only upload if cache is is out of date
 		if not is_cache_in_date(root, cachepath):
-				
-			#get the last set of commits
-			git = Git(root)
 			gitDetails = "<strong>"+gitName+"</strong>"
 
-				try:
-					logs =  git.log('--pretty=oneline', '--since=1.hour').split('\n')
-					gitDetails = gitDetails + "<br>" + "<br>".join(logs)
-				except:
-					pass
+			try:
+                        	logs =  git.log('--pretty=oneline', '--since=1.hour').split('\n')
+				gitDetails = gitDetails + "<br>" + "<br>".join(logs)
+                	except:
+                        	pass
 
-				arrUpdatedRepos.append(gitDetails)
-				create_cache(root, cachepath)
-				#need to upload
-				upload_cache(cachepath, pathRel)
+			print gitDetails
+
+			arrUpdatedRepos.append(gitDetails)
+			create_cache(root, cachepath)
+			#need to upload
+			upload_cache(cachepath, pathRel)
 
 print "Completed check..."
 conn.close()
 
 #notify of changes
 if len(arrUpdatedRepos) > 0:
-        import smtplib
-
-        SMTP_SERVER = 'smtp.gmail.com'
-        SMTP_PORT = 587
-
-        subject = 'Git Repo Back Up'
-        body = "Modified Repos:<br><br>"
-        body = body + "<br><br>".join(arrUpdatedRepos)
-
-        headers = ["From: " + emailSender,
+	import smtplib
+ 
+	SMTP_SERVER = 'smtp.gmail.com'
+	SMTP_PORT = 587
+ 
+	subject = 'Git Repo Back Up'
+	body = "Modified Repos:<br><br>"
+	body = body + "<br><br>".join(arrUpdatedRepos)
+ 
+	headers = ["From: " + emailSender,
            "Subject: " + subject,
            "To: " + emailRecipient,
            "Mime-Version: 1.0",
            "Content-Type: text/html;"]
 
-        headers = "\r\n".join(headers)
+	headers = "\r\n".join(headers)
+ 
+	session = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+ 
+	session.ehlo()
+	session.starttls()
+	session.ehlo
+	session.login(emailUser, emailPassword)
+ 
+	session.sendmail(sender, recipient, headers + "\r\n\r\n" + body)
+	session.quit()
 
-        session = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
 
-        session.ehlo()
-        session.starttls()
-        session.ehlo
-        session.login(emailUser, emailPassword)
-
-        session.sendmail(sender, recipient, headers + "\r\n\r\n" + body)
-        session.quit()
